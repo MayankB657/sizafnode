8f9a6d15a371199cbe225e172a0a1fe1264bef44
Mayank Bhandure <mayu.bhandure657@gmail.com>
2024-09-06 17:46:39 +0530

    updated packeges

--- server.mjs
+++ server.mjs
@@ -1,9 +1,11 @@
 import http from 'http';
 
+const heartEmoji = String.fromCodePoint(0x2764); // Unicode code point for the heart emoji
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello Monika\nMade with ❤ from Aibuzzz....");
+  res.setHeader("Content-Type", "text/plain; charset=utf-8"); // Specify UTF-8 encoding
+  res.end("Hello Monika\nMade with ❤️ from Aibuzzz....");
 });
 
 const PORT = process.env.PORT || 3000;
